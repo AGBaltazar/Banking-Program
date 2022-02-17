8c7c403ac8c2af34849d62fdf940e94081a08e6c
Alejandro Garcia Baltazar <agee.baltazar@gmail.com>
2022-02-17 06:04:52 -0800

    Commit to Main

--- bankAccount.cpp
+++ bankAccount.cpp
@@ -11,12 +11,23 @@ class bankAccount {
         double accountBalance, accountInterest;
         int accountNumber;
 
+    //Function will get user input and add it to the set variables to later be accessed
     void openAccount(){
         cout << "\nEnter Account Number:   ", cin >> accountNumber;
         cout << "\nEnter Your Name:  ", cin >> accountName;
-        cout << "\nWould this be a Checkings or Savings Account  ", cin >> accountType;
+        cout << "\nWould this be a Checkings or Savings Account:  ", cin >> accountType;
         cout << "\nEnter the Initial Balance:  ", cin >> accountBalance;
         cout << "\nEnter Current Interest Rate:  ", cin >> accountInterest;
+        cout << "\n";
+    }
+
+    void viewAccount(){
+        cout << "\nAccount Number: " << accountNumber;
+        cout << "\nAccount Name: " << accountName;
+        cout << "\nAccount Type: " << accountType;
+        cout << "\nAccount Balance: " << accountBalance;
+        cout << "\nAccount Interest: %" << accountInterest;
+        cout << "\n";
     }
 };
 
@@ -28,8 +39,9 @@ int main() {
     do
     {
         cout << "\n Please select the following options ";
-        cout << "\n 1:Open Account";
-        cout << "\n 2: Exit \n";
+        cout << "\n 1: Open Account";
+        cout << "\n 2: View Account";
+        cout << "\n 3: Exit \n\n";
 
         cin >> choice;
 
@@ -38,8 +50,12 @@ int main() {
         case '1':
             bankAcc.openAccount();
             break;
-        
+
         case '2':
+            bankAcc.viewAccount();
+            break;
+        
+        case '3':
             break;
 
         }
